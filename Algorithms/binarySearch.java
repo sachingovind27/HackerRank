2543deb964c2fbd60e0ce7019b39501fb898082e
Sachin Govind <sachingovind27@gmail.com>
2021-10-04 01:39:05 +0530

    recursive function complete

--- Algorithms/binarySearch.java
+++ Algorithms/binarySearch.java
@@ -1,17 +1,29 @@
 package Algorithms;
 
 class binarySearch{
-    binarySearch bst = new binarySearch();
-    int median(){
-        return     
-    }
 
-    int searchBST(){
-        int begin=0,end=,index =  0;
+    //global input
+    static int []input = new int[100];
+    static binarySearch bst = new binarySearch();
+    int searchNum;
+
+    int searchBST( int begin,int end,int index){
+       
+        if(searchNum==input[index]){
+            return index;
+        }
+
+        if(searchNum<input[index]){
+            index = searchBST(begin,index-1,index/2);
+        }else{
+            index = searchBST(index+1,end,index/2);
+        }
 
         return index;
     } 
     public static void main(){
+
+        bst.searchBST(0,input.length,input.length/2);
         System.out.println("Start test1");
     }
 }
