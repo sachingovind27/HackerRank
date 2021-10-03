f5c7f71f87c67dc80d99d76f26e2e26c1198d01f
Sachin Govind <sachingovind27@gmail.com>
2021-10-04 01:22:26 +0530

    recursive -1

--- Algorithms/binarySearch.java
+++ Algorithms/binarySearch.java
@@ -1,6 +1,16 @@
 package Algorithms;
 
 class binarySearch{
+    binarySearch bst = new binarySearch();
+    int median(){
+        return     
+    }
+
+    int searchBST(){
+        int begin=0,end=,index =  0;
+
+        return index;
+    } 
     public static void main(){
         System.out.println("Start test1");
     }
