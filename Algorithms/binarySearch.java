c7948559362860db5a8e55262b04cd2447dabae9
Sachin Govind <sachingovind27@gmail.com>
2021-10-04 01:11:48 +0530

    input for bst

--- Algorithms/binarySearch.java
+++ Algorithms/binarySearch.java
@@ -1,7 +1,34 @@
 package Algorithms;
 
+import java.util.*;
+
 class binarySearch{
     public static void main(){
-        System.out.println("Start test1");
+
+       //Take input 
+       Scanner sc = new Scanner(System.in); 
+       int []input = new int[100];
+
+       try {
+           
+            String []inpStr = sc.nextLine().split(",");
+            int i =0 ;
+
+            for(String str : inpStr){
+                input[i] = Integer.parseInt(str);
+            }
+
+       } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+
+       }finally{
+            sc.close();
+       }
+      
+       //call Search
+
+      
     }
 }
